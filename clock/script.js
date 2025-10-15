b7019a17f3dea4ea2d9d6f57b12f4a01793d59c2
Jyotishman Das <jyotishmand78@gmail.com>
2025-10-15 22:57:19 +0530

    Fix hour hand variable name and update min hand color in clock implementation

--- clock/script.js
+++ clock/script.js
@@ -1,4 +1,4 @@
-let hour = document.querySelector("#hourhand");
+let hourr = document.querySelector("#hourhand");
 let min = document.querySelector("#minhand");
 let sec = document.querySelector("#sechand")
 
@@ -12,10 +12,15 @@ let date = () => {
     const minDegress = ((minutes / 60) * 360) + 90;
     const hourDegrees = ((hour / 12) * 360) + 90;
 
+    if (secondDegrees === 90) {
+        sec.style.transition = "none";
+    } else {
+        sec.style.transition = "all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)";
+    }
 
     sec.style.transform = `rotate(${secondDegrees}deg)`;
     min.style.transform = `rotate(${minDegress}deg)`;
-    hour.style.transform = `rotate(${hourDegrees}deg)`;
+    hourr.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(date, 1000)
